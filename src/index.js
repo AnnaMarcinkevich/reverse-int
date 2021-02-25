eb6bbc9c6a794cfdefea5b2900d984bf3c359f76
AnnaMarcinkevich <anna.martsynkevich2020@gmail.com>
2021-02-25 23:44:30 +0300

    feat: function implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let str1 = String(Math.abs(n));
+    let str2 = "";
+    for (let i = str1.length - 1; i >= 0; i--) {
+        str2 += str1[i];
+    }
+    return Number(str2);
+};
